1f5807b217cc7e6d59348faa5f4dfebc5d4d61f4
PabloFCavadore <[pfcavadore@gmail.com]>
2025-04-21 19:30:52 -0300

    Add project files.

---
+++ Controllers/GenerosController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/generos")]
+    [ApiController]
+
+    public class GenerosController: ControllerBase
+    {
+        private readonly IRepositorio repositorio;
+
+        public GenerosController(IRepositorio repositorio) 
+        {
+            this.repositorio = repositorio;
+        }
+
+        [HttpGet]
+        [HttpGet("listado")]
+        [HttpGet("/listado-generos")]
+        [OutputCache]
+        public List<Genero>Get() 
+        {
+            
+            var generos = repositorio.ObtenerTodosLosGeneros();
+
+            return generos;
+        }
+
+        [HttpGet("{id:int}")]
+        [OutputCache]
+        public async Task<ActionResult<Genero>> Get(int id) 
+        {
+            
+            var genero = await repositorio.ObtenerPorId(id);
+            
+            if (genero is null)
+            {
+                return NotFound();
+            }
+
+            return genero;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Genero genero)
+        {
+            var repositorio = new RepositorioEnMemoria();
+            var yaExisteElGenero = repositorio.Existe(genero.Nombre);
+            if (yaExisteElGenero) 
+            {
+                return BadRequest($"Ya existe un genero con el nombre {genero.Nombre}");
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public void PUT()
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete()
+        {
+
+        }
+    }
+}
